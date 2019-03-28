618355aaaf1c35c6e1e978a1744d468ecc8b70b6
Dorohi <m.m.dorohi@gmail.com>
2019-03-28 15:08:33 +0200

    Вызов и закрытие модального окна popup

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 'use strict';
 
 window.addEventListener('DOMContentLoaded', () => {
+
+	// Модальное окно "Вызова инженера"
 	const engineerButton = document.querySelector('.popup_engineer_btn'),
 		popupEngineer = document.querySelector('.popup_engineer');
 
@@ -10,11 +12,31 @@ window.addEventListener('DOMContentLoaded', () => {
 
 	popupEngineer.addEventListener('click', (event) => {
 		const target = event.target;
-		console.log(target);
+		
 		if (target.classList.contains('popup_close') ||
 			target.parentNode.classList.contains('popup_close')||
 			target.classList.contains('popup_engineer')) {
 			popupEngineer.style.display = 'none';
 		}
 	});
+
+	//Модальное окно "Обратного звонка"
+	const phoneLink = document.querySelectorAll('.phone_link'),
+		popupModal = document.querySelector('.popup');
+	
+	phoneLink.forEach(element => {
+		element.addEventListener('click', () => {
+			popupModal.style.display = "block";
+		});
+	});
+	popupModal.addEventListener('click', (event) => {
+		const target = event.target;
+		
+		if (target.classList.contains('popup_close') ||
+			target.parentNode.classList.contains('popup_close') ||
+			target.classList.contains('popup')) {
+			popupModal.style.display = 'none';
+		}
+	});
+
 });
