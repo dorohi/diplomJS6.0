0b8c951e7a7f8a62c1734c399215cb1614f00933
Dorohi <m.m.dorohi@gmail.com>
2019-03-29 12:27:59 +0200

    Табы: Закажите отделку

--- js/script.js
+++ js/script.js
@@ -7,14 +7,14 @@ window.addEventListener('DOMContentLoaded', () => {
 		popupEngineer = document.querySelector('.popup_engineer');
 
 	engineerButton.addEventListener('click', () => {
-		popupEngineer.style.display = "block";		
+		popupEngineer.style.display = "block";
 	});
 
 	popupEngineer.addEventListener('click', (event) => {
 		const target = event.target;
-		
+
 		if (target.classList.contains('popup_close') ||
-			target.parentNode.classList.contains('popup_close')||
+			target.parentNode.classList.contains('popup_close') ||
 			target.classList.contains('popup_engineer')) {
 			popupEngineer.style.display = 'none';
 		}
@@ -23,7 +23,7 @@ window.addEventListener('DOMContentLoaded', () => {
 	//Модальное окно "Обратного звонка"
 	const phoneLink = document.querySelectorAll('.phone_link'),
 		popupModal = document.querySelector('.popup');
-	
+
 	phoneLink.forEach(element => {
 		element.addEventListener('click', () => {
 			popupModal.style.display = "block";
@@ -31,7 +31,7 @@ window.addEventListener('DOMContentLoaded', () => {
 	});
 	popupModal.addEventListener('click', (event) => {
 		const target = event.target;
-		
+
 		if (target.classList.contains('popup_close') ||
 			target.parentNode.classList.contains('popup_close') ||
 			target.classList.contains('popup')) {
@@ -39,31 +39,17 @@ window.addEventListener('DOMContentLoaded', () => {
 		}
 	});
 
-	//Tabs
+	//ТАБЫ ОСТЕКЛЕНИЕ БАЛКОНОВ И ЛОДЖИЙ
 
 	const glazingSlider = document.querySelector('.glazing_slider'),
 		glazingBlock = document.querySelectorAll('.glazing_block'),
 		glazingRow = document.querySelectorAll('.glazing .row');
 
-	hideTabContent(1);
-
-	function hideTabContent(key) {
-		for (let i = key; i < glazingRow.length; i++) {
-			glazingRow[i].classList.remove('show');
-			glazingRow[i].classList.add('hide');
-		}
-	}
-
-	function showTabContent(key) {
-		if (glazingRow[key].classList.contains('hide')) {
-			glazingRow[key].classList.remove('hide');
-			glazingRow[key].classList.add('show');
-		}
-	}
+	hideTabContent(1, glazingRow);
 
 	glazingSlider.addEventListener('click', function (event) {
 		let target = event.target;
-		if (!target.classList.contains('glazing_block')){
+		if (!target.classList.contains('glazing_block')) {
 			target = target.parentNode;
 		}
 
@@ -71,12 +57,55 @@ window.addEventListener('DOMContentLoaded', () => {
 			for (let i = 0; i < glazingBlock.length; i++) {
 				glazingBlock[i].querySelector('a').classList.remove('active');
 				if (glazingBlock[i] == target) {
-					hideTabContent(0);
-					showTabContent(i);
+					hideTabContent(0, glazingRow);
+					showTabContent(i, glazingRow);
 				}
 			}
 			target.querySelector('a').classList.add('active');
 		}
 	});
+
+	// ТАБЫ ЗАКАЖИТЕ ОТДЕЛКУ БАЛКОНА СО СКИДКОЙ 60%!
+
+	const decorationSlider = document.querySelector('.decoration_slider'),
+		decorationItem = document.querySelectorAll('.decoration_item'),
+		decorationRow = document.querySelectorAll('.decoration_row');
+
+	hideTabContent(1, decorationRow);
+
+	decorationSlider.addEventListener('click', function (event) {
+		let target = event.target;
+		if (!target.classList.contains('no_click')) {
+			target = target.parentNode;
+		}
+		target = target.parentNode;
+
+		if (!target.querySelector('div').classList.contains('after_click')) {
+			for (let i = 0; i < decorationItem.length; i++) {
+				decorationItem[i].querySelector('div').classList.remove('after_click');
+				decorationItem[i].querySelector('div').classList.add('no_click');
+				if (decorationItem[i] == target) {
+					hideTabContent(0, decorationRow);
+					showTabContent(i, decorationRow);
+				}
+			}
+			target.querySelector('div').classList.add('after_click');
+			target.querySelector('div').classList.remove('no_click');
+		}
+	});
+
+	function hideTabContent(key, slideItems) {
+		for (let i = key; i < slideItems.length; i++) {
+			slideItems[i].classList.remove('show');
+			slideItems[i].classList.add('hide');
+		}
+	}
+
+	function showTabContent(key, slideItems) {
+		if (slideItems[key].classList.contains('hide')) {
+			slideItems[key].classList.remove('hide');
+			slideItems[key].classList.add('show');
+		}
+	}
 
 });
