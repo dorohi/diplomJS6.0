6f844a039503d8217d5bbc030e41f78b183ddb40
Dorohi <m.m.dorohi@gmail.com>
2019-03-29 15:41:19 +0200

    Задел для таймера

--- js/script.js
+++ js/script.js
@@ -113,4 +113,51 @@ window.addEventListener('DOMContentLoaded', () => {
 		}
 	}
 
+	// ТАЙМЕР СКИДОК 
+
+	const deadLine = '2019-05-04';
+
+	function getTimeRemaining(endTime) {
+		const t = Date.parse(endTime) - Date.parse(new Date());
+		const seconds = Math.floor(t / 1000 % 60),
+			minutes = Math.floor(t / 1000 / 60 % 60),
+			hours = Math.floor(t / 1000 / 60 / 60 % 60),
+			days = Math.floor(t / 1000 / 60 / 60 / 24 % 24);
+			console.log(days + ' ' + hours + ' ' + minutes + ' ' + seconds);
+			return {
+			'total': t,
+			'days': days,
+			'hours': hours,
+			'minutes': minutes,
+			'seconds': seconds
+		};
+	}
+
+	function setClock(id, endTime) {
+		const timer = document.getElementById(id),
+			days = timer.querySelector('#days'),
+			hours = timer.querySelector('#hours'),
+			minutes = timer.querySelector('#minutes'),
+			seconds = timer.querySelector('#seconds'),
+			timeInterval = setInterval(update, 1000);
+
+		function update() {
+			var t = getTimeRemaining(endTime);
+			days.textContent = getNormal(t.days);
+			hours.textContent = getNormal(t.hours);
+			minutes.textContent = getNormal(t.minutes);
+			seconds.textContent = getNormal(t.seconds);
+
+			if (t.total <= 0) {
+				clearInterval(timeInterval);
+			}
+		}
+	}
+
+	setClock('timer', deadLine);
+
+	function getNormal(number) {
+		return number < 10 ? '0' + number : number;
+	}
+
 });
